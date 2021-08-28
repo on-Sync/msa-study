e4cb95f974ffa4dca3f3ee80dca1fde5da605b32
on-Sync <putstack.career@gmail.com>
2021-08-29 04:15:13 +0900

    caching events - JVM GC 전까지 유지됨.

--- src/main/java/com/putstack/msa_order_service_command/config/AxonConfig.java
+++ src/main/java/com/putstack/msa_order_service_command/config/AxonConfig.java
@@ -6,6 +6,8 @@ import java.util.List;
 import com.putstack.msa_order_service_command.aggregate.OrderAggregate;
 
 import org.axonframework.commandhandling.SimpleCommandBus;
+import org.axonframework.common.caching.Cache;
+import org.axonframework.common.caching.WeakReferenceCache;
 import org.axonframework.common.transaction.TransactionManager;
 import org.axonframework.eventsourcing.AggregateFactory;
 import org.axonframework.eventsourcing.AggregateSnapshotter;
@@ -16,7 +18,6 @@ import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
 import org.axonframework.eventsourcing.Snapshotter;
 import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.modelling.command.Repository;
-import org.axonframework.spring.stereotype.Aggregate;
 import org.axonframework.springboot.autoconfig.AxonAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
@@ -42,6 +43,7 @@ public class AxonConfig {
     @Bean
     public List<AggregateFactory<?>> aggregateFactories(){
         List<AggregateFactory<?>> aggregateFactories = new ArrayList<AggregateFactory<?>>();
+        // INFO: add Aggregate.class to list
         aggregateFactories.add(new GenericAggregateFactory<>(OrderAggregate.class));
         return aggregateFactories;
     }
@@ -64,13 +66,18 @@ public class AxonConfig {
         return new EventCountSnapshotTriggerDefinition(snapshotter, SNAPSHOT_THRESHOLD);
     }
 
-    @Bean
-    public Repository<OrderAggregate> OrderAggregateRepository(EventStore eventStore, SnapshotTriggerDefinition snapshotTriggerDefinition){
+    @Bean // INFO: Snapshot repository - for custom aggregate
+    public Repository<OrderAggregate> orderAggregateRepository(EventStore eventStore, SnapshotTriggerDefinition snapshotTriggerDefinition){
         return EventSourcingRepository
                 .builder(OrderAggregate.class)
                 .eventStore(eventStore)
                 .snapshotTriggerDefinition(snapshotTriggerDefinition)
                 .build();
+    }
+
+    @Bean // INFO: Enhance read functionality by axon cache
+    public Cache cache(){
+        return new WeakReferenceCache();
     }
 
 }
