8b4efb07c7fe92c94a7c9affc83cd173f7e2343c
on-Sync <putstack.career@gmail.com>
2021-08-29 02:37:47 +0900

    aggregate snapshot / threshold=5

--- src/main/java/com/putstack/msa_order_service_command/config/AxonConfig.java
+++ src/main/java/com/putstack/msa_order_service_command/config/AxonConfig.java
@@ -1,7 +1,22 @@
 package com.putstack.msa_order_service_command.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.putstack.msa_order_service_command.aggregate.OrderAggregate;
+
 import org.axonframework.commandhandling.SimpleCommandBus;
 import org.axonframework.common.transaction.TransactionManager;
+import org.axonframework.eventsourcing.AggregateFactory;
+import org.axonframework.eventsourcing.AggregateSnapshotter;
+import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.EventSourcingRepository;
+import org.axonframework.eventsourcing.GenericAggregateFactory;
+import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.Snapshotter;
+import org.axonframework.eventsourcing.eventstore.EventStore;
+import org.axonframework.modelling.command.Repository;
+import org.axonframework.spring.stereotype.Aggregate;
 import org.axonframework.springboot.autoconfig.AxonAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
@@ -13,11 +28,49 @@ import lombok.extern.log4j.Log4j2;
 @AutoConfigureAfter(AxonAutoConfiguration.class)
 @Log4j2
 public class AxonConfig {
+
+    private final static int SNAPSHOT_THRESHOLD = 5; 
+    
     @Bean
-    SimpleCommandBus commandBus(TransactionManager transactionManager) {
+    public SimpleCommandBus commandBus(TransactionManager transactionManager) {
         log.debug("config {}", "AxonServerCommandBus -> SimpleCommandBus");
         return SimpleCommandBus.builder()
             .transactionManager(transactionManager)
             .build();
     }
+
+    @Bean
+    public List<AggregateFactory<?>> aggregateFactories(){
+        List<AggregateFactory<?>> aggregateFactories = new ArrayList<AggregateFactory<?>>();
+        aggregateFactories.add(new GenericAggregateFactory<>(OrderAggregate.class));
+        return aggregateFactories;
+    }
+
+    @Bean
+    public Snapshotter snapshotter(EventStore eventStore, TransactionManager transactionManager){
+
+        List<AggregateFactory<?>> aggregateFactories = aggregateFactories();
+
+        return AggregateSnapshotter
+                .builder()
+                .eventStore(eventStore)
+                .aggregateFactories(aggregateFactories)
+                .transactionManager(transactionManager)
+                .build();
+    }
+
+    @Bean
+    public SnapshotTriggerDefinition snapshotTriggerDefinition(Snapshotter snapshotter){
+        return new EventCountSnapshotTriggerDefinition(snapshotter, SNAPSHOT_THRESHOLD);
+    }
+
+    @Bean
+    public Repository<OrderAggregate> OrderAggregateRepository(EventStore eventStore, SnapshotTriggerDefinition snapshotTriggerDefinition){
+        return EventSourcingRepository
+                .builder(OrderAggregate.class)
+                .eventStore(eventStore)
+                .snapshotTriggerDefinition(snapshotTriggerDefinition)
+                .build();
+    }
+
 }
